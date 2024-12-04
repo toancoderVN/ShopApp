58f3143c873faa81013f1517e0352cff21330164
ToanCoderVN <toansos2002@gmail.com>
2024-12-05 00:19:50 +0700

    Push Repository

--- src/main/java/com/example/shopapp/repositories/RoleRepository.java
+++ src/main/java/com/example/shopapp/repositories/RoleRepository.java
@@ -3,6 +3,6 @@ package com.example.shopapp.repositories;
 import com.example.shopapp.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
